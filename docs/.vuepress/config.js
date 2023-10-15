5baffee0b490c41fc1fd1046a163d1c911db8d5d
azil <854257920@qq.com>
2023-10-15 16:12:10 +0800

    commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: '粉粉蕉的笔记本',
   description: 'Just playing around',
-  base: '/note/',  //基础路径
+  //base: '/note/',  //基础路径
   head: [
     ['link',{rel: 'icon', href: '/images/favicon.ico'}],  //网站图标
   ],
